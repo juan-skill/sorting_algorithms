e8888e4feed5ee0d4d51e683236ebc9e26072227
juan-skill <1294@holbertonschool.com>
2020-02-04 03:45:55 +0000

    [Fix] correction eficiency algorithm 	modified:   0-bubble_sort.c

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -20,11 +20,11 @@ void bubble_sort(int *array, size_t size)
 	while (flag)
 	{
 		flag = 0;
-		for (i = 1; i < size; i++)
+		for (i = 0; i < size - 1; i++)
 		{
-			if (array[i - 1] > array[i])
+			if (array[i] > array[i + 1])
 			{
-				swap(&array[i], &array[i - 1]);
+				swap(&array[i], &array[i + 1]);
 				print_array(array, size);
 				flag = 1;
 			}
